91addeb1f166fcf79b4e1c71136f19782967afdc
Jonathan Harris <jonathan@marginal.org.uk>
2018-11-18 11:41:30 +0000

    Code tidy

--- RadarAltitude/RadarAltitude.cs
+++ RadarAltitude/RadarAltitude.cs
@@ -3,6 +3,13 @@ using KSP.UI.Screens.Flight;
 
 namespace RadarAltitude
 {
+    static class RDA1
+    {
+        public const double maxRange = 3000;	// Maximum range of RDA-1 in-cockpit instrument
+        public const double mediumRez = 1000;	// Medium resolution between 1000m and 500m
+        public const double highRez = 500;	// Highest resolution below 500m
+    }
+
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class RadarAltitude : MonoBehaviour
     {
@@ -22,11 +29,11 @@ namespace RadarAltitude
             {
                 altTumbler.tumbler.SetColor(Color.black);
             }
-            else if (radarAlt < 3000)		// Maximum range of RDA-1 in-cockpit instrument
+            else if (radarAlt < RDA1.maxRange)		// Maximum range of RDA-1 in-cockpit instrument
             {
-                if (radarAlt > 1000)		// Low resolution between 1000-3000m
+                if (radarAlt > RDA1.mediumRez)		// Low resolution between 1000-3000m
                     radarAlt = ((int) radarAlt / 10) * 10;
-                else if (radarAlt > 500)	// Reduced resolution between 500-1000m
+                else if (radarAlt > RDA1.highRez)	// Reduced resolution between 500-1000m
                     radarAlt = ((int) radarAlt / 5) * 5;
 
                 altTumbler.tumbler.SetValue(radarAlt);
